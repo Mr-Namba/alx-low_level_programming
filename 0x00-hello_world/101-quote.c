cdc57f763a15519f8dadccec1235f819700f76aa
Percy Namba <percy.namba@yahoo.com>
2023-03-16 18:06:18 +0000

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	char quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	for (int i = 0; quote[i] != '\0'; +ii)
+	for (int i = 0; quote[i] != '\0'; ++i)
 		putchar(quote[i]);
 	return (1);
 }
