4c94ebca808cfebe411d83d1cda0631d7039e20b
Percy Namba <percy.namba@yahoo.com>
2023-03-17 17:27:05 +0000

    print some alphabets

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int base_10;
+
+	base_10 = 0;
+	while (base_10 < 10; ++base_10)
+		printf("%d\n");
+	return (0);
+}
