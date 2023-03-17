cc8a47864e629d9bf94387c6bfb1305979393f8b
Percy Namba <percy.namba@yahoo.com>
2023-03-17 23:42:38 +0000

    combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,9 @@ int main(void)
 {
 	int i, j;
 
-	i = '0';
-	j = '0';
-	for (i <= '9'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (j < '9'; j++)
+		for (j = '0'; j <= '9'; j++)
 			putchar(i);
 			putchar(',');
 			putchar(j);
