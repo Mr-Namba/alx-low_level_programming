15dbb2b9cbd31872613675771509b43dc4b7cd5f
Percy Namba <percy.namba@yahoo.com>
2023-03-17 14:40:59 +0000

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char alpha, l_alpha;
+
+	alpha = 'A';
+	l_alpha = tolower(alpha);
+	for (l_alpha = 'a'; l_alpha <= 'z'; ++l_alpha)
+		putchar(l_alpha);
+	return (0);
+
+
