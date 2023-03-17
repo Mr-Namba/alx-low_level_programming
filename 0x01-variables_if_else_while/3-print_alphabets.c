45c68bec2244e9b3e107abf17a02bf7cf04c2041
Percy Namba <percy.namba@yahoo.com>
2023-03-17 16:19:55 +0000

    upper and lower

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	int alhpa, l_alpha, u_alpha;
+
+	alpha = 'A';
+	l_alpha = tolower(alpha);
+	u_alpha = toupper(l_alpha);
+	for (l_alpha = 'a'; l_alpha <= 'z'; ++l_alpha)
+		putchar(l_alpha);
+	for (u_alpha = 'A'; u_alpha <= 'Z'; ++u_alpha)
+		putchar(u_alpha);
+	putchar('\n');
+	return (0);
+}
