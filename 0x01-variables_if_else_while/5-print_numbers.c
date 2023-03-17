9070dc8245141cddca754f9567707fa8847f91c6
Percy Namba <percy.namba@yahoo.com>
2023-03-17 18:02:55 +0000

    numberz

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,8 +9,11 @@ int main(void)
 	int base_10;
 
 	base_10 = 0;
-	while (base_10 < 10, base_10++)
+	while (base_10 < 10)
+	{
 		printf("%d", base_10);
+		base_10++;
+	}
 	putchar('\n');
 	return (0);
 }
