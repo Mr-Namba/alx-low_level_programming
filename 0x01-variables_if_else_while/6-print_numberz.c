4110430df4d7b89e717340481ac49dc3f47dee76
Percy Namba <percy.namba@yahoo.com>
2023-03-17 18:18:11 +0000

    some numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int base_10;
+
+	base_10 = 0;
+
+	while (base_10 > 10)
+	{
+		putchar(base_10);
+		base_10++;
+	}
+	putchar('\n');
+	return (0);
+}
