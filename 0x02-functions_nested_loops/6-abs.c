57bb5e279b89d4804e2f1929ded8b00b90712309
Percy Namba <percy.namba@yahoo.com>
2023-03-21 18:34:40 +0000

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main"
+/**
+ * _abs - finds the absolute value of the number
+ * @int: is the number to be made absilute
+ * Return: always 0 (success)
+ */
+int _abs(int)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
