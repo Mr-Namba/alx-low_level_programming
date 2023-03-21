174a3e5d025fb7fc77a80e08dad25be3d8eb648c
Percy Namba <percy.namba@yahoo.com>
2023-03-21 15:43:33 +0000

    10 line lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet in lower case 10 times
+ *
+ * Return: always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 10)
+	{
+		char c, cl;
+
+		c = 'A';
+		cl = tolower(c);
+		while (cl <= 'z')
+		{
+			_putchar(cl);
+			cl++;
+		}
+		i++;
+	}
