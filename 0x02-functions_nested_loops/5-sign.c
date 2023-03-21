444663b1c893c443cad7fafb1aef40ff50d2d5a3
Percy Namba <percy.namba@yahoo.com>
2023-03-21 17:37:38 +0000

    checks sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,13 +5,20 @@
  * Return: 1 or 0 or -1
  */
 int print_sign(int n)
+{
 	if (n >= 1)
+	{
 		return (1);
 		_putchar('+');
+	}
 	else if (n == 0)
+	{
 		return (0);
 		_putchar('0');
+	}
 	else
+	{
 		return (-1);
 		_putchar('-');
+	}
 }
