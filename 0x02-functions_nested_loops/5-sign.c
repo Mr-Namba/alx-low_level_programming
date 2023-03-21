1d4d7570ce03b2a5c5b747d504ee02bcf80120ca
Percy Namba <percy.namba@yahoo.com>
2023-03-21 17:30:50 +0000

    checks sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: the number to be checked
+ * Return: 1 or 0 or -1
+ */
+int print_sign(int n)
+	if (n >= 1)
+		return (1);
+		_putchar('+');
+	else if (n == 0)
+		return (0);
+		_putchar('0');
+	else
+		return (-1);
+		_putchar('-');
+}
