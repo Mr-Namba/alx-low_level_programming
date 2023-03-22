b5f7523bed80e350eb96072269d394c2bd5ceeb3
Percy Namba <percy.namba@yahoo.com>
2023-03-22 17:04:15 +0000

    comment

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop
+* main - stops an infinite loop
+* \o/: happy to skip inite loop
 * Return: 0
 */
 
@@ -10,9 +11,9 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
+	i = 0;
 /**
-*	i = 0;
-*
 *	while (i < 10)
 *	{
 *	putchar(i);
