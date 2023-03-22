1256028568c654b45809d37ce691f1f733dac8ff
Percy Namba <percy.namba@yahoo.com>
2023-03-22 16:27:05 +0000

    mulriple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - tests function if integer is positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	int j;
+
+	int k;
+
+	i = 4;
+	j = -6;
+	k = 0;
+	positive_or_negative(i);
+
+	positive_or_negatice(j);
+
+	positive_or_negative(k);
+
+	return (0);
+}
