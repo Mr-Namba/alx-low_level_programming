857334d25b543709e9f8938b925aad6b6843ce58
Percy Namba <percy.namba@yahoo.com>
2023-03-23 18:23:41 +0000

    print 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - prints out numbers fro 0 to 9
+ *
+ * Return: always 0 (success)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
