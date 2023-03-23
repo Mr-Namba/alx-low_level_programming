3072bbbd198645542d6276ec13458031ca64e73c
Percy Namba <percy.namba@yahoo.com>
2023-03-23 18:40:50 +0000

    some numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints out some numbers
+ *
+ * Result: always 0 (success)
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+	if (i >= 0 && i <= 9 && i != 2 && i != 4)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	putchar('\n');
+}
