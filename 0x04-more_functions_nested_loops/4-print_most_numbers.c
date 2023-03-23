ae1a1bf620e80c535fec2b1439062de0602fc6dd
Percy Namba <percy.namba@yahoo.com>
2023-03-23 20:15:23 +0000

    some numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,10 @@ void print_most_numbers(void)
 {
 	int i;
 
-	i = 0;
-	if (i >= 0 && i <= 9 && i != 2 && i != 4)
+	for (i = 0; i <= 9; i++)
 	{
-		_putchar(i + '0');
-		i++;
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
 	}
 	_putchar('\n');
 }
