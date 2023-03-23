d7ebf1948534a7df23820d110cca7438ee453800
Percy Namba <percy.namba@yahoo.com>
2023-03-23 17:45:44 +0000

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * *mul - multiplies two integers
+ * @a: character to be multiplied
+ * @b: chararacter to be multiplied
+ * Return: product of two
+ */
+int mul(int a, int b);
+{
+	return (a * b);
+}
