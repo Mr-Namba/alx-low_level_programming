9e6a900cd48c4a0fe139588351971ed153e01795
Percy Namba <percy.namba@yahoo.com>
2023-03-24 22:28:54 +0000

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square - prints out a square
+ * @size: the size of the square
+ * Return: alwasy 0 (success)
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 0; i <= size; i++)
+		{
+			for (j = 0; j <= size; j++)
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
