5b990ed4326b83a54cdf9e62531ba8e558f1d8a0
Percy Namba <percy.namba@yahoo.com>
2023-03-24 06:52:50 +0000

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_diagonal - prints line in a diagonal
+ * @n: is the number of times / is printed
+ * Return: always 0 (success)
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+			_putchar('\');
+		_puchar('\n');
+	}
+}
