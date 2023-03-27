ad0bcd795cb3372de64409cc26aab14ba430b8d5
Percy Namba <percy.namba@yahoo.com>
2023-03-27 17:32:09 +0000

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_rev - print in reverse
+ * @s: the string
+ * Return: always 0 success
+ */
+void print_rev(char *s)
+{
+	while (*s < '\0')
+	{
+		_putchar(*S0);
+		s--;
+	}
+	_putchar('\n');
+}
