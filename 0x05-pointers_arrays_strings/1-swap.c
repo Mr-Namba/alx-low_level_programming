f9eaa344baa71445ac96e9e535d63a75a2627743
Percy Namba <percy.namba@yahoo.com>
2023-03-27 12:18:29 +0000

    swap them

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swaps two values
+ * @a: first value to be swapped
+ * @b: second value to be swapped
+ * Return: always 0 (success)
+ */
+void swap_int(int *a, int *b)
+{
+	*a = b;
+
+	*b = a;
+}
