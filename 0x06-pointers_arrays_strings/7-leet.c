198bbaae0a5d38752bc8fd94d4782c47518092cf
Percy Namba <percy.namba@yahoo.com>
2023-03-30 15:52:08 +0000

    swap

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,27 +1,28 @@
 #include "main.h"
-/**
- * leet - replaces some alphabets
- * @a: the string
- * Return: return string
- */
-char *leet(char *a)
-{
-	int i;
 
-	for (i = 0; a[i] != '\0'; i++)
+/**
+ * leet - encodes a string in 1337
+ * @s: string to be encoded
+ *
+ * Return: result
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (a[i] == 'a' || a[i] == 'A')
-			a[i] = 4;
-		else if (a[i] == 'e' || a[i] == 'E')
-			a[i] = 3;
-		else if (a[i] == 'o' || a[i] == 'O')
-			a[i] = 0;
-		else if (a[i] == 't' || a[i] == 'T')
-			a[i] = 7;
-		else if (a[i] == 'l' || a[i] == 'L')
-			a[i] = 1;
-		else
-			a[i] = a[i];
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
 	}
-	return (a);
+
+	return (s);
 }
