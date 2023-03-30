547335cbb939bfb56b54840e5c3debca54acf398
Percy Namba <percy.namba@yahoo.com>
2023-03-30 10:11:54 +0000

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,26 +7,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j, result;
-
-	i = 0;
-	j = 0;
-	result = 0;
-	while (s1[i] != '\0' && s2[j] != '\0')
+	while (*s1 == *s2)
 	{
-		i++;
-		j++;
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
 	}
-	if ((int)s1[0] > (int)s2[0])
-	{
-		result = (int)s1[0] - (int)s2[0]
-		return (result);
-	}
-	else if ((int)s1[0] == (int)s2[0])
-		return (0);
-	else if ((int)s1[0] > (int)s2[0])
-	{
-		result = (int)s1[0] - (int)s2[0];
-		return (result);
-	}
+	return (*s1 - *s2);
 }
