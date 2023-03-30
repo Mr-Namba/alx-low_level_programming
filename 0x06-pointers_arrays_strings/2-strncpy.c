b94bfed22e5074349ac25ba3acb9ec8821b8aea0
Percy Namba <percy.namba@yahoo.com>
2023-03-30 09:59:20 +0000

    copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,9 +16,10 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i] = src[i];
 		i++;
 	}
-	if (i == n && src[i] != '\0')
+	while (i < n)
 	{
-		dest[i - 1] = '\0';
+		dest[i] = '\0';
+		i++;
 	}
 	return (dest);
 }
