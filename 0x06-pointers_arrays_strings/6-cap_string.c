148a01dfe9007300bda36994f10cfdf1529af5cd
Percy Namba <percy.namba@yahoo.com>
2023-03-30 13:57:48 +0000

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *a)
 
 	if (isalpha(a[0]))
 		a[0] = toupper(a[0]);
-	for (i = 0; a[i] != '\0'; i++)
+	for (i = 1; a[i] != '\0'; i++)
 	{
 		if (!isalpha(a[i - 1]) && !isdigit(a[i - 1]) && isalpha(a[i]))
 			a[i] = toupper(a[i]);
