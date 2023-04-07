88b1e3fda5d40fe39a4566210ffa3f8ba078965b
Percy Namba <percy.namba@yahoo.com>
2023-04-07 12:57:01 +0000

    len

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,13 +6,6 @@
  */
 int _strlen_recursion(char *s)
 {
-	int n;
-
-	n = 0;
 	if (*s != '\0')
-	{
-		n++;
-		_strlen_recursion(s + 1);
-		return (n);
-	}
+		return (1 + _strlen_recursion(s + 1));
 }
