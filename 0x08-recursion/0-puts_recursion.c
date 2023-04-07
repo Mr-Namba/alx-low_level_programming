408f468663167ff7f44230ff513c01bed4fa72a3
Percy Namba <percy.namba@yahoo.com>
2023-04-07 10:32:56 +0000

    put

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints using recursion
+ * @s: the string
+ * Return: it will be void
+ */
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	if (i < '\0')
+	{
+		_putchar(s[i]);
+		i++;
+		_puts_recursion(s);
+	}
+	_putchar('\n');
+}
