0158566b4478bfcfcbd01c57beb2b9796fffdb53
Percy Namba <percy.namba@yahoo.com>
2023-04-07 15:08:53 +0000

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - square rootin using recursion
+ * @n: the number
+ * Return: give out the square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (int _pow_recursion(n, 0.5));
+}
