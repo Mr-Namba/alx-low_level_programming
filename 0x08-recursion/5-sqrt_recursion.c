03f88350429841980e5404d4d234d6399c915752
Percy Namba <percy.namba@yahoo.com>
2023-04-07 15:33:34 +0000

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,27 @@
 #include "main.h"
 /**
+ * firststep - the first step to the solution
+ * @n: the number to be raised
+ * @i: the square root checker
+ * Return: the final answer
+ */
+int firststep(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	else if (i * i == n)
+		return (i);
+	return (firststep(n, i + 1));
+}
+/**
  * _sqrt_recursion - square rootin using recursion
  * @n: the number
  * Return: give out the square root
  */
+
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (_pow_recursion(n, 0.5));
+	return (firststep(n, 0));
 }
