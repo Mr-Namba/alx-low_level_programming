44b0f3f9c49390c56ad5d9bcb7d93cf0639a2f64
Percy Namba <percy.namba@yahoo.com>
2023-04-09 05:08:46 +0000

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,16 +1,28 @@
 #include "main.h"
 /**
+ * to_check - checks the string
+ * @s: the string
+ * @length: the lenghth
+ * i@: to go through the string
+ * Return: 1 or 0
+ */
+int to_check(char *s, int i, int length)
+{
+	if (*(s + i) != *(s + length - 1))
+		return (0);
+	if (i >= length)
+		return (1);
+	return (to_check(s, i + 1, length - 1));
+}
+/**
  * is_palindrome - confirms is a string is palindrome
  * @s: the string
  * Return: returns integers
  */
 int is_palindrome(char *s)
 {
-	char *r;
+	if (*s == 0)
+		return (0);
+	return (to_check(s, 0, _strlen_recursion(s)));
 
-	r = _print_rev_recursion(s);
-	if (r == s)
-		return (1);
-	return (0);
 }
-
