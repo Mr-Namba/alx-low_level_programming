ab8d58ce1c7980afc015e1f8ded41334bb3a4640
Percy Namba <percy.namba@yahoo.com>
2023-04-09 05:23:36 +0000

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,9 +1,21 @@
 #include "main.h"
 /**
+ * _strlen_recursion - finds the length of a string
+ * @s: the string
+ * Return: the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+/**
  * to_check - checks the string
  * @s: the string
  * @length: the lenghth
- * i@: to go through the string
+ * @i: to go through the string
  * Return: 1 or 0
  */
 int to_check(char *s, int i, int length)
