b6bc06f7438afbe2c230b02bd37fad9ef8196de9
Percy Namba <percy.namba@yahoo.com>
2023-04-11 14:12:44 +0000

    14mallocs

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * create_array - create array
+ * @size: size of the array
+ * @c: characters of the sring
+ * Return: null or pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+
+	int i;
+
+	if (size == 0)
+		return (NULL);
+	arr = malloc(sizeof(c) * size);
+
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+	return (arr);
+}
