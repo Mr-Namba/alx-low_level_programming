5df2c73184b1e4b58f637b3783f46456c5ee9656
Percy Namba <percy.namba@yahoo.com>
2023-04-18 11:14:15 +0000

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: the array of parameters
  * Return: 0 for success
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc _attribute_((unused)), char *argv[])
 {
 	printf("%s\n", arg[0]);
 	return (0);
