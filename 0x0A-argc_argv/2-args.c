8e8bd8b8d807b8b12597cf64faac07bb927ef549
Percy Namba <percy.namba@yahoo.com>
2023-04-18 11:49:44 +0000

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,11 +5,11 @@
  * @argv: the array of parameters
  * Return: 0 for succes
  */
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
-		printf("%s", argv[i]);
+		printf("%s\n", argv[i]);
 	return (0);
 }
