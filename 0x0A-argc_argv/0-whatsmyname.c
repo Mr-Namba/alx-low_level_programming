ea03bf8d67c2051edf3aae775c045a520bbed6ee
Percy Namba <percy.namba@yahoo.com>
2023-04-18 11:03:40 +0000

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: the count of arguments
+ * @argv: the array of parameters
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", arg[0]);
+	return (0);
+}
