53b85eb219a9c8e122c76becdd4ac877369861b8
Percy Namba <percy.namba@yahoo.com>
2023-04-18 13:30:28 +0000

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: the number of parameters
+ * @argv: the array of parameters
+ * Return: 0 for success
+ */
+int main(int argc, char *argv)
+{
+	int i;
+
+	int j;
+
+	int sum;
+
+	sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+		printf("%d\n", sum);
+	}
+	return (0);
+}
