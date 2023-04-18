693db16d2e6f4dbda6938c1eab4aedfe3d65d77a
Percy Namba <percy.namba@yahoo.com>
2023-04-18 11:34:53 +0000

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: count of arguments
+ * @argv: array of arguments
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+}
