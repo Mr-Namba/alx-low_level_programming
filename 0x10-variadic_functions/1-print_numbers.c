4036391019e0cb9de5089558bf3cde79dd0536ab
Percy Namba <percy.namba@yahoo.com>
2023-04-20 21:53:16 +0000

    task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - to print numbers in the string
+ * @separator: the string to seaparate numbers
+ * @n: the number integers paseed to the function
+ * Return: 0 for success
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	usigned int i;
+	va_list p;
+
+	va_start(p, n);
+
+	if (seperator == NULL)
+		seperator = "";
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(p, int));
+		if (i < n - 1)
+			printf("%s", seperator);
+	}
+	printf("\n");
+	va_end(p);
+}
